3cccb498872afccd758e55c29d778c00a3850e2c
rianyunandar <rianyunandar@gmail.com>
2021-02-06 17:49:47 +0700

    Add Brand Logo And Motto

--- mobile/Screen/DrawerScreens/HomeScreen.js
+++ mobile/Screen/DrawerScreens/HomeScreen.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { Text,
+import React, { Component } from "react";
+import {
+  Text,
   FlatList,
   Image,
   StyleSheet,
@@ -7,74 +8,118 @@ import { Text,
   View,
   ScrollView,
   TextInput
-} from 'react-native';
-var {height, width } = Dimensions.get('window');
-import Swiper from 'react-native-swiper'
-import api from '../../api'
-import Axios from 'axios'
-export default class App extends Component {
+} from "react-native";
+var { height, width } = Dimensions.get("window");
+const a_proportion = '70%';
+const b_proportion = '20%';
 
-  constructor(props)
-  {
+import Swiper from "react-native-swiper";
+import api from "../../api";
+import Axios from "axios";
+export default class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      dataBanner:[]
-    }
+      dataBanner: []
+    };
   }
 
-  componentDidMount(){
-    const url = `${api}/menus` 
+  componentDidMount() {
+    const url = `${api}/menus`;
     Axios.get(url, {
       timeout: 20000
-  }).then((response)=>{
-      console.log(response.data.bannerList)
-
-      this.setState({
-        isLoading: false,
-        dataBanner: response.data.bannerList,
-      });
-      
     })
-    .catch((error) =>{
-      console.error(error);
-    });
+      .then((response) => {
+        this.setState({
+          isLoading: false,
+          dataBanner: response.data.bannerList
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
     return (
       <ScrollView>
-       {/* { Object.entries(this.state.dataBanner).map((postData) => {
-        console.log(postData) })} */}
-        <View style={{ flex: 1,backgroundColor:"#f2f2f2" }}>
-          <View style={{width: width, alignItems:'center'}} >
-              <Image style={{height:60,width:width/2,margin:10 }} resizeMode="contain" source={require("../../Image/Logo.png")}  />
-              <Swiper style={{height:width/2}}  showsButtons={false} autoplay={true} autoplayTimeout={2}>
-                {
-                  this.state.dataBanner.map(Object=>{
-                    console.log(Object);
-                    return(
-                      <Image key={Object.id} style={styles.imageBanner} resizeMode="contain" source={{uri:Object.img_url}}/>
-                    )
-                  })
-                }
-              </Swiper>
-              <View style={{height:20}} />
+        <View style={styles.container}>
+          <View style={styles.centerView}>
+            <Swiper
+              style={{ height: width / 2 }}
+              showsButtons={false}
+              autoplay={true}
+              autoplayTimeout={2}
+            >
+              {this.state.dataBanner.map((Object) => {
+                // console.log(Object);
+                return (
+                  <Image
+                    key={Object.id}
+                    style={styles.imageBanner}
+                    resizeMode="contain"
+                    source={{ uri: Object.img_url }}
+                  />
+                );
+              })}
+            </Swiper>
+            <View style={{ height: 10 }} />
           </View>
         </View>
-       
+          <View style={styles.normalView}>
+            <Image
+              style={styles.logoBanner}
+              source={require("../../Image/Logo.png")}
+            />
+            <Text style={styles.textBanner}>Martabak Bangka Anugrah Koga</Text>
+            <Text style={styles.mottoBanner}>We Always Serve The Best</Text>
+
+          </View>
       </ScrollView>
-    )
-              
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#112263",
+  },
+  normalView: {
+    padding:20,
+    width: width,
+    alignContent: "stretch",
+    flexWrap:"wrap",
+    flexDirection:"row"
+  },
+  centerView: {
+    width: width,
+    alignItems: "center"
+  },
+  imageBanner: {
+    height: width / 2,
+    width: width - 40,
+    borderRadius: 10,
+    marginHorizontal: 20
+  },
+  logoBanner: {
+    maxHeight: 60,
+    maxWidth:b_proportion,
+    margin: 10,
+  },
+  textBanner: {
+    fontFamily: "sans-serif-medium",
+    fontSize: 18,
+    textTransform: "uppercase",
+    color: "red",
+    alignSelf:"center",
+    maxWidth:a_proportion,
 
 
-
-  }}
-
-  const styles = StyleSheet.create({
-    imageBanner: {
-      height:width/2,
-      width:width-40,
-      borderRadius:10,
-      marginHorizontal:20
-    }, 
-  });
+  },
+  mottoBanner: {
+    fontFamily: "sans-serif-medium",
+    color: "#cdaa7c",
+    marginHorizontal:10
+  }
+});
