ff17cf39141ee0f9c8190f1adddbfc39899a0279
rianyunandar <rianyunandar@gmail.com>
2021-02-07 10:23:07 +0700

    detail screen beta Done

--- mobile/Screen/DrawerScreens/DetailsScreen.js
+++ mobile/Screen/DrawerScreens/DetailsScreen.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ScrollView
+} from "react-native";
 import api from "../../api";
 import 'intl';
 import 'intl/locale-data/jsonp/id-ID'; // or any other locale you need
 import Loader from '../Components/Loader';
 import Axios from "axios";
 import AsyncStorage from '@react-native-community/async-storage';
+var { height, width } = Dimensions.get("window");
 
-
- export default class DetailsScreen extends React.Component {
+const numberFormat = (value) =>
+  new Intl.NumberFormat('locales', {
+    style: 'currency',
+    currency: 'IDR'
+  }).format(value);
+ export default class DetailsScreen extends Component {
 
   // const menuId = useNavigationParam('key');
   // const menuId = route.params;
@@ -17,9 +29,9 @@ import AsyncStorage from '@react-native-community/async-storage';
     super(props);
     this.state = {
       menuID: getId,
-      dataDetail: [],
+      dataDetail: '',
       loading:true,
-      userToken:null
+      userToken:""
     }}
   
      getToken = async () => {
@@ -37,7 +49,7 @@ import AsyncStorage from '@react-native-community/async-storage';
     } 
     componentDidMount() {
       // this.getToken()
-      console.log(this.state.userToken)
+      // console.log(this.state.userToken)
       const params = this.state.menuID;
       const url = `${api}/menus/id/${params}`;
       Axios.get(url, {
@@ -50,7 +62,7 @@ import AsyncStorage from '@react-native-community/async-storage';
             loading: false,
             dataDetail: response.data
           });
-          // console.log(this.state.dataMenus)
+          // console.log(response.data)
         })
         .catch((error) => {
           console.error(error);
@@ -59,43 +71,65 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 
   render() {
-    console.log(this.state.menuID)
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{flex: 1, padding: 16}}>
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text
-            style={{
-              fontSize: 20,
-              textAlign: 'center',
-              marginBottom: 16,
-            }}>
-            Example of Splash, Login and Sign Up in React Native 
-            {'\n\n'}
-            This is the Details Screen  
-          </Text>
-        </View>
-        <Text
-          style={{
-            fontSize: 18,
-            textAlign: 'center',
-            color: 'grey',
-          }}>
-          Splash, Login and Register Example{'\n'}React Native
-        </Text>
-        <Text
-          style={{
-            fontSize: 16,
-            textAlign: 'center',
-            color: 'grey',
-          }}>
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
-}}
+    // <FlatList
+    // data={this.state.dataDetail}
+    // renderItem={({ item }) => 
+    // (
+      console.log(this.state.dataDetail)
+      return(
+        <View style={styles.container}>
+         <Image style={styles.imageBanner} 
+          source={{ uri: this.state.dataDetail.url_image }} />
+         <Text style={styles.menuHeader}>
+           {this.state.dataDetail.menu_tittle}
+         </Text>
+         <View style={styles.hrLine}/>
+         <Text style={styles.menuDescList}>Deskripsi : {this.state.dataDetail.desc2}</Text>
+         <Text style={styles.menuDescList}>Bahan Utama : {this.state.dataDetail.desc1}</Text>
+         <Text style={styles.menuDescList}>Harga : {`${(numberFormat(this.state.dataDetail.price))}`}</Text>
+       </View>
+    )
+  //  }
+  //   keyExtractor = { (Object,index) => index.toString() }
+  //  /> // console.log(this.state.menuID)
+   
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#112263",
+    padding:10,
+    width: width,
+    alignContent: "center",
+  },
+ 
+  imageBanner: {
+    height: width / 2,
+    width: width - 40,
+    borderRadius: 10,
+    // margin: 20
+    alignSelf:"center"
+  
+  },
+  menuHeader: {
+    fontFamily: "sans-serif-condensed",
+    color: "white",
+    fontWeight:"bold",
+    textTransform: "uppercase",
+    fontSize:20,
+    alignSelf:"center",
+    padding:10
+  },
+  
+  menuDescList:{
+    fontSize:18,
+    color:"white",
+    marginTop:10,
+    alignSelf:"stretch",
+    marginHorizontal:10
+  },
+  hrLine:{borderBottomColor:"#999",borderBottomWidth:5}
+
+});
