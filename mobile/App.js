7251a326264236e999407d2b08a699ef39d7933b
rianyunandar <rianyunandar@gmail.com>
2021-02-07 14:39:23 +0700

    debug registrasi header

--- mobile/App.js
+++ mobile/App.js
@@ -32,7 +32,7 @@ const Auth = () => {
         options={{
           title: 'Register', //Set Header Title
           headerStyle: {
-            backgroundColor: '#307ecc', //Set Header color
+            backgroundColor: '#112263', //Set Header color
           },
           headerTintColor: '#fff', //Set Header text color
           headerTitleStyle: {
